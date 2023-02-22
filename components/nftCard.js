bceda58e90cca607cbbd9a644341bdee88575a1b
Sina Rahimi <sinarahimi.main@gmail.com>
2023-02-22 20:58:19 +0100

    media bug still

--- components/nftCard.js
+++ components/nftCard.js
@@ -61,14 +61,18 @@ export const NFTCard = ({nft}) => {
                         </div>
                     </div>
 
-                    <div className="w-full text-center"> 
-                        {
-                            router.pathname == "/my-nfts" &&
-                            <div className='flex'>
-                                <button type="submit" onClick={write} className="w-1/3 text-white rounded-sm bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium text-md px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">List</button>
-                                <input type="number" onChange={e => setListingPrice(e.target.value)} className="w-2/3 text-center bg-gray-50 border border-gray-300 text-gray-900 text-sm focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 rounded-sm" placeholder="Price in Matic"></input>
-                            </div>
+                    <div className="w-full text-center">
+                        {isLoading && 
+                        <p>Listing your NFT...</p>
                         }
+                        {isSuccess && 
+                        <p>Still working...</p>
+                        }
+                        
+                        <div className='flex'>
+                            <button type="submit" onClick={write} className="w-1/3 text-white rounded-sm bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium text-md px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">List</button>
+                            <input type="number" onChange={e => setListingPrice(e.target.value)} className="w-2/3 text-center bg-gray-50 border border-gray-300 text-gray-900 text-sm focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 rounded-sm" placeholder="Price in Matic"></input>
+                        </div>
                             {/* <button className="py-2 w-full text-white bg-blue-500">Buy</button>
                             :
                             router.pathname == "/my-nfts" & isLoading ?
