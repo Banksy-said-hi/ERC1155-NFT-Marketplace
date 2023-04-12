35276e75926cec35222d9fd8a2d01e2e1a53829a
Sina Rahimi <sinarahimi.main@gmail.com>
2023-04-12 17:22:51 +0200

    Commit before mailing project to Media

--- components/buyNFT.js
+++ components/buyNFT.js
@@ -51,7 +51,7 @@ const Buy = (props) => {
                 isLoading ? <p className="bg-blue-700 text-white text-center py-2.5">Buying this item...</p> :
                     isSuccess ? <p className="bg-blue-700 text-white text-center py-2.5">Still working...</p> :
                         <div>
-                            <button onClick={write} disabled={!write} className="w-full text-center text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium text-md px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Buy {amount} tokens</button>
+                            <button onClick={write} disabled={!write} className="w-full text-center text-white bg-blue-500 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium text-md px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Buy {amount} tokens</button>
                             <div className="flex bg-slate-100 justify-center items-center">
                                 <input value={amount} type="range" min="1" max={props.totalAmount} onChange={(e) => setAmount(e.target.value)} className="w-full mx-1 mt-3 mb-2"></input>
                             </div>
